e317fc8032881f55dbaf54e2daf9c16904345d16
Jed Watson <jed.watson@me.com>
2021-06-30 05:06:56 +1000

    Step 4

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,32 @@
 import { Container } from '../components/ui/layout';
 import { H1 } from '../components/ui/typography';
+import { fetchGraphQL, gql } from '../utils';
 
-export default function Home() {
+export default function Home({ posts }: any) {
   return (
     <Container>
       <H1>Hello World</H1>
+      <ul>
+        {posts.map((post: any) => (
+          <li>{post.title}</li>
+        ))}
+      </ul>
     </Container>
   );
+}
+
+export async function getStaticProps() {
+  const data = await fetchGraphQL(gql`
+    query {
+      allPosts {
+        id
+        title
+        status
+        author {
+          name
+        }
+      }
+    }
+  `);
+  return { props: { posts: data.allPosts }, revalidate: 60 };
 }
