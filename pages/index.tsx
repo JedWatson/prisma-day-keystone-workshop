e363bb96f685b1af12d9227d06aab3a2cd99dfac
Jed Watson <jed.watson@me.com>
2021-06-30 05:00:56 +1000

    Step 3

---
+++ pages/index.tsx
@@ -0,0 +1,10 @@
+import { Container } from '../components/ui/layout';
+import { H1 } from '../components/ui/typography';
+
+export default function Home() {
+  return (
+    <Container>
+      <H1>Hello World</H1>
+    </Container>
+  );
+}
